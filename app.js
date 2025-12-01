08a5d0bce233b6676fe8acbaf350ac4c1cd7e8f3
오세민 <68673741+starlunar88@users.noreply.github.com>
2025-12-01 11:08:27 +0900

    Update app.js

--- app.js
+++ app.js
@@ -750,15 +750,8 @@ async function getSystemSettings() {
     const defaultSettings = {
         courtCount: 3,
         timeSlots: [
-            { start: "09:00", end: "10:00" },
-            { start: "10:00", end: "11:00" },
-            { start: "11:00", end: "12:00" },
             { start: "12:00", end: "13:00" },
             { start: "13:00", end: "14:00" },
-            { start: "14:00", end: "15:00" },
-            { start: "15:00", end: "16:00" },
-            { start: "16:00", end: "17:00" },
-            { start: "17:00", end: "18:00" },
             { start: "18:00", end: "19:00" },
             { start: "19:00", end: "20:00" }
         ],
